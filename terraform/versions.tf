8229cfd0f275146fcc39e493b08449e8eaf694db
awsplayground19892-sys <awsplayground19892@gmail.com>
2025-07-19 22:09:01 +0800

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "~> 1.9.5"
   backend "s3" {
-    bucket         = "gitops-tf-backend"
+    bucket         = "dev-params-tfstate"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "GitopsTerraformLocks"
